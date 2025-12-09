a24866447d6436ea306e0c7b87cec3e9f845e84d
VenimK <venimkmac@gmail.com>
2025-12-09 07:55:54 +0100

    Remove obsolete dark mode files and database config
    CLEANUP: Removed old dark mode implementation files 🧹
    
    DELETED FILES:
    - custom-dark-mode.css (old dark mode stylesheet)
    - dbsettings.inc.php (database config - should not be in repo)
    - install-darkmode.sh (obsolete installer script)
    
    These files are superseded by the new theme system with:
    - Theme customizer (create themes visually)
    - Theme upload tool (web-based management)
    - Multiple pre-installed themes (Dark, Dracula, etc.)

--- dbsettings.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dbsettings['engine'] = 'pdo_mysql';
-$dbsettings['host'] = 'localhost';
-$dbsettings['dbname'] = 'spotweb';
-$dbsettings['user'] = 'spotweb';
-$dbsettings['pass'] = 'spotweb';
-$dbsettings['port'] = '8889';
-$dbsettings['schema'] = '';
