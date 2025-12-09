5278f9eaec6d6d38fda55e1681ccfe5342cab8b7
VenimK <venimkmac@gmail.com>
2025-12-09 07:40:13 +0100

    Remove obsolete documentation and debug files
    CLEANUP: Removed outdated dark mode docs and debug scripts 🧹
    
    DELETED FILES:
    - DARKMODE-README.md (obsolete dark mode docs)
    - check-retrieve-simple.php (debug script)
    - debug-performance.php (debug script)
    - deploy-darkmode-to-ct108.sh (old deployment script)
    - deploy-darkmode-to-ct102.sh (old deployment script)
    - deploy-theme-tools.sh (superseded by new tools)
    - install-darkmode.sh (obsolete installer)
    - test-retrieve-*.php (multiple debug scripts)
    - Various

--- view-log.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-Type: text/plain");
-$logFile = __DIR__ . "/install-errors.log";
-if (file_exists($logFile)) {
-    echo file_get_contents($logFile);
-} else {
-    echo "Log file not found.";
-}
